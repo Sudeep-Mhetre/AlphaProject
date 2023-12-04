ce92c6a2bdef4b7cef13e7c361919d51c63fd0cc
unknown <er.sudeepmhetre@gmail.com>
2023-12-04 20:39:30 +0530

    First Commit

---
+++ frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-property-card',
+   // template: '<h1>I am a card<h1>'
+   templateUrl: 'property-card.component.html',
+   styleUrls : ['property-card.component.css'],
+}
+)
+
+export class PropertyCardComponent{
+       Property : any = {
+        "Id" : 1,
+        "Name" : "Sai Sadan",
+        "Type" : "Residential building",
+        "Price" : 12000
+       }
+     }
